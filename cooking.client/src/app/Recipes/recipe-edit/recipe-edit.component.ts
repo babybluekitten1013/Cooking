4cc8d626634eea1145a54b3c3ad4a2e1c1bb90bf
Sarah Harlan <baby.blue.kitten1013@gmail.com>
2025-06-10 19:10:09 -0700

    feat: started update recipe form, need to finish this.

--- cooking.client/src/app/Recipes/recipe-edit/recipe-edit.component.ts
+++ cooking.client/src/app/Recipes/recipe-edit/recipe-edit.component.ts
@@ -1,7 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormControl, Validators } from '@angular/forms';
 import { DataService, MeasurementIngredient, Recipe } from '../../data.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,15 +10,33 @@ import { ActivatedRoute, Router } from '@angular/router';
   templateUrl: './recipe-edit.component.html',
   styleUrl: './recipe-edit.component.css'
 })
-export class RecipeEditComponent {
+export class RecipeEditComponent implements OnInit{
   fb = inject(FormBuilder);
-  dataservice = inject(DataService);
+  dataService = inject(DataService);
   route = inject(ActivatedRoute);
   router = inject(Router);
 
-  ingredients = this.dataservice.ingredients$;
-  measurements = this.dataservice.measurements$;
+  id: number | null = null;
 
+  ingredients = this.dataService.ingredients$;
+  measurements = this.dataService.measurements$;
+
+  ngOnInit(): void {
+    this.dataService.recipes$.pipe(
+      map((recipes: Recipe[]) => recipes.filter(recipe => recipe.recipeID == this.id)[0]),
+      tap(val => console.log("Selected Recipe", val)),
+    ).subscribe(data => {
+      if (!data) {
+        this.initEmptyForm();
+      } else {
+        this.initExistingForm(data);
+      }
+    })
+
+    this.route.paramMap.subscribe(params => {
+      this.id = Number(params.get("id"));
+    })
+  }
 
   //form group include validators
   recipeForm = this.fb.group({
@@ -33,30 +52,71 @@ export class RecipeEditComponent {
     measurementIngredients: this.fb.array([])
   })
 
+  initEmptyForm() {
+    this.recipeForm.patchValue({
+      recipeID: 0,
+      recipeName: "",
+      recipeTag: "",
+      description: "",
+      instructions: "",
+      prepTime: "",
+      cookTime: "",
+      yields: 0,
+      image: "",
+      measurementIngredients: []
+    })
+  }
+
+  initExistingForm(recipe: Recipe) {
+    this.recipeForm.patchValue({
+      recipeID: recipe.recipeID,
+      recipeName: recipe.name,
+      recipeTag: recipe.recipeTag,
+      description: recipe.description,
+      instructions: recipe.instructions,
+      prepTime: recipe.prepTime,
+      cookTime: recipe.cookTime,
+      yields: recipe.yield,
+      image: recipe.imageURL,
+      measurementIngredients: []
+    });
+
+    recipe.measurementIngredients.forEach(mi => {
+      this.measureIngred.push(this.fb.group({
+        measurementIngredientID: [mi.measurementIngredientId, [Validators.required]],
+        measurementID: [mi.measurementId, [Validators.required]],
+        ingredientID: [mi.ingredientId, [Validators.required]],
+        recipeID: [mi.recipeID, [Validators.required]],
+        quantity: [mi.quantity, [Validators.required]],
+        details: [mi.details, [Validators.required]]
+      }))
+    })
+  }
+
   //on submit function
   onSubmit(): void {
     console.log("Form Submitted!")
     console.log(this.recipeForm.value)
 
-    //let newRecipe: Recipe = {
-    //    recipeID: Number(this.recipeForm.value.recipeID),
-    //    name: this.recipeForm.value.recipeName!,
-    //    recipeTag: this.recipeForm.value.recipeTag!,
-    //    description: this.recipeForm.value.description!,
-    //    instructions: this.recipeForm.value.instructions!,
-    //    prepTime: this.recipeForm.value.prepTime!,
-    //    cookTime: this.recipeForm.value.cookTime!,
-    //    yield: this.recipeForm.value.yields!,
-    //    imageURL: this.recipeForm.value.image!,
-    //    measurementIngredients: this.recipeForm.value.measurementIngredients as MeasurementIngredient[]
-    //}
+    let newRecipe: Recipe = {
+        recipeID: Number(this.recipeForm.value.recipeID),
+        name: this.recipeForm.value.recipeName!,
+        recipeTag: this.recipeForm.value.recipeTag!,
+        description: this.recipeForm.value.description!,
+        instructions: this.recipeForm.value.instructions!,
+        prepTime: this.recipeForm.value.prepTime!,
+        cookTime: this.recipeForm.value.cookTime!,
+        yield: this.recipeForm.value.yields!,
+        imageURL: this.recipeForm.value.image!,
+        measurementIngredients: this.recipeForm.value.measurementIngredients as MeasurementIngredient[]
+    }
 
-    //this.dataservice.createRecipe(newRecipe).subscribe(data => {
-    //  console.log("Created:", data)
-    //  this.dataservice.getAllRecipes();
-    //  this.router.navigate(["Recipes", data.recipeID]);
+    this.dataService.createRecipe(newRecipe).subscribe(data => {
+      console.log("Created:", data)
+      this.dataService.getAllRecipes();
+      this.router.navigate(["Recipes", data.recipeID]);
       
-    //});
+    });
   };
   //initalize form function
   OnInit(): void {
